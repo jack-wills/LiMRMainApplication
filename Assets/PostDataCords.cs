70a357042d98c4dde6135dc71479c768e3237ab5
Balaji <sharanblj08@gmail.com>
2020-04-12 18:34:56 +0100

    Completed drone + Posting of Coordinates to AWS (Use IKJL to move drone and P to post data to cloud)

---
+++ Assets/PostDataCords.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Text;
+
+public class PostDataCords : MonoBehaviour
+{
+    public GameObject drone;
+    private Vector3 Cords;
+    private Vector3 Rots;
+
+    private string url = "https://bmgxwpyyd2.execute-api.us-east-1.amazonaws.com/prod/sensordata";
+    public string sensorID;
+    private string value;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        drone = GameObject.Find("AxisLockedDrone");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Cords = drone.transform.position;
+        Rots = drone.transform.eulerAngles;
+
+        //value = writeVectorProperly(Cords);
+        
+    }
+
+   
+
+    public void SendCords()
+    {
+        
+        StartCoroutine(PostRequest(url, Cords, Rots));
+    }
+
+    IEnumerator PostRequest(string uri, Vector3 position, Vector3 rotation)
+    {
+
+        var request = new UnityWebRequest(url, "POST");
+        byte[] bodyRaw = Encoding.UTF8.GetBytes("{\"timestamp\": \"" + System.DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss") + "\", \"sensorValues\": \"{\\\"Drone.Roty\\\":\\\"" + rotation.y + "\\\", \\\"Drone.Posy\\\":\\\"" + position.y + "\\\", \\\"Drone.Rotx\\\":\\\"" + rotation.x + "\\\"}\" }");
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+        yield return request.SendWebRequest();
+
+        string[] pages = uri.Split('/');
+        int page = pages.Length - 1;
+
+        if (request.isNetworkError)
+        {
+            Debug.LogError(pages[page] + ": Error: " + request.error);
+        }
+        else
+        {
+            Debug.Log(pages[page] + ":\nReceived: " + request.downloadHandler.text);
+        }
+    }
+
+    public string writeVectorProperly(Vector3 temp)
+    {
+        string final;
+        final = temp.ToString("F3");
+        Debug.Log(final);
+
+        return final;
+    }
+
+    public void ButtonPress()
+    {
+        StartCoroutine(PostRequest(url));
+    }
+
+    IEnumerator PostRequest(string uri)
+    {
+
+        var request = new UnityWebRequest(url, "POST");
+        //byte[] bodyRaw = Encoding.UTF8.GetBytes("{\"timestamp\": \"" + System.DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss") + "\", \"sensorValues\": \"{\\\"" + sensorID + "\\\":\\\"" + value + "\\\"}\" }");
+        byte[] bodyRaw = Encoding.UTF8.GetBytes("{\"timestamp\": \"" + System.DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss") + "\", \"sensorValues\": \"{\\\"" + sensorID + "\\\":\\\"" + value + "\\\"}\" }");
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+        yield return request.SendWebRequest();
+
+        string[] pages = uri.Split('/');
+        int page = pages.Length - 1;
+
+        if (request.isNetworkError)
+        {
+            Debug.LogError(pages[page] + ": Error: " + request.error);
+        }
+        else
+        {
+            Debug.Log(pages[page] + ":\nReceived: " + request.downloadHandler.text);
+        }
+    }
+
+}
---
+++ Assets/Post_Cords_ALDrone.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Text;
+
+
+using UnityEngine;
+
+public class Post_Cords_ALDrone : MonoBehaviour
+{
+
+    public PostDataCords postData;
+
+    private string url = "https://bmgxwpyyd2.execute-api.us-east-1.amazonaws.com/prod/sensordata";
+    private string droneID;
+    private string height;
+    public GameObject Drone;
+
+
+    private void Start()
+    {
+        Drone = GameObject.Find("AxisLockedDrone");
+        Drone.SetActive(true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.P))
+        {
+            //Was used for testing Drone.SetActive(true);
+            //postData.ButtonPress();
+            postData.SendCords();
+            
+        }
+
+    }
+
+ 
+
+}
+
