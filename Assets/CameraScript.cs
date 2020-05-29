0577779134f0c9f065ff45e36f5e10c541ce3ee9
Jack Williams <jackw53519@gmail.com>
2020-05-29 11:21:43 +0100

    Improved camera movements for filming

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+
+    void LateUpdate()
+    {
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, target.position, smoothSpeed);
+        transform.position = smoothedPosition;
+        Quaternion smoothedRotation = Quaternion.Lerp(transform.rotation, target.rotation, smoothSpeed);
+        transform.rotation = smoothedRotation;
+    }
+}
